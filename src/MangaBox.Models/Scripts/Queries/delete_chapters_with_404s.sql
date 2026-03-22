dab2387b9d419966b0c097948beace7eb86fbe3c
calico-crusade <holycardboardquest@gmail.com>
2026-03-22 03:22:19 -0400

    Fixing query cuz me dumb

--- src/MangaBox.Models/Scripts/Queries/delete_chapters_with_404s.sql
+++ src/MangaBox.Models/Scripts/Queries/delete_chapters_with_404s.sql
@@ -1,13 +1,13 @@
-WITH chapter_logs AS (
+WITH chapters_404 AS (
     SELECT
         DISTINCT
-        TRIM(substring(message FROM 'Fetch: (.*?)"'))::uuid as chapterId
+        TRIM(substring(message FROM 'Fetch: (.*?)"')) as chapterId
     FROM mb_logs
     WHERE
         source = 'MangaBox.Utilities.MangaDex.MangaDexService' AND
         message LIKE 'Manga Dex Error: "Pages Fetch: %" - "not_found_http_exception%' AND
         created_at > (CURRENT_TIMESTAMP - INTERVAL '1 days')
-    UNION
+), chapters_no_pages AS (
     SELECT
         DISTINCT
         TRIM(SUBSTRING(message, 24, 36))::uuid as chapterId
@@ -21,9 +21,20 @@ WITH chapter_logs AS (
     SELECT
         c.id as chapter_id
     FROM mb_chapters c
-    JOIN chapter_logs l ON l.chapterId = c.source_id
+    JOIN chapters_404 l ON l.chapterId = c.source_id
     JOIN mb_manga m ON m.id = c.manga_id
-    WHERE c.deleted_at IS NULL AND m.deleted_at IS NULL
+    WHERE
+        c.deleted_at IS NULL AND
+        m.deleted_at IS NULL
+    UNION
+    SELECT
+        c.id as chapter_id
+    FROM mb_chapters c
+    JOIN chapters_no_pages l ON l.chapterId = c.id
+    JOIN mb_manga m ON m.id = c.manga_id
+    WHERE
+        c.deleted_at IS NULL AND
+        m.deleted_at IS NULL
 ), do_updates AS (
     UPDATE mb_chapters c
     SET deleted_at = CURRENT_TIMESTAMP
